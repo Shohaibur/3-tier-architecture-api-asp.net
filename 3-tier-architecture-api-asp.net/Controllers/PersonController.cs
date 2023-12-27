4c845680173ae778f3e9675adcc5a0f7d21fe1c9
Shoaib <shoaibrochi9@gmail.com>
2023-12-27 23:54:02 +0600

    Add project files.

---
+++ 3-tier-architecture-api-asp.net/Controllers/PersonController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace _3_tier_architecture_api_asp.net.Controllers
+{
+    public class PersonController : ApiController
+    {
+        //get person api
+        [HttpGet]
+        [Route("api/demo")]
+        public HttpResponseMessage Get()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+    }
+}
---
+++ 3-tier-architecture-api-asp.net/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_3_tier_architecture_api_asp.net.WebApiApplication" Language="C#" %>
